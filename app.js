c74ee75df252679799a9ba7d13f9ca0d9ab5419e
Tomytool <56263086+Tomytool@users.noreply.github.com>
2021-10-05 01:02:51 -0300

    Desafio nuemro tres de JS

---
+++ app.js
@@ -0,0 +1,36 @@
+function opcionFibonacci() {
+  let fibonacci1 = 1;
+  let fibonacci2 = 2;
+  let resultadoFibonacci;
+  const calculoFibonacci = parseInt(
+    prompt('ingrese el numero Fibonacci a Calcular:')
+  );
+
+  for (let index = 3; index < calculoFibonacci; index++) {
+    resultadoFibonacci = fibonacci1 + fibonacci2;
+    fibonacci1 = fibonacci2;
+    fibonacci2 = resultadoFibonacci;
+  }
+  console.log(resultadoFibonacci);
+}
+
+function impares() {
+  const calculoImpares = parseInt(prompt('Ingrese un numero del 1 al 100: '));
+  for (let index = 1; index <= calculoImpares; index++) {
+    if (index % 2 != 0) {
+      console.log(index);
+    }
+  }
+}
+function factorial() {
+  let resultadoFactorial = 1;
+  const calcularFactorial = parseInt(
+    prompt('Ingrese el nuemro Factorial a calcular: ')
+  );
+  for (let index = 1; index <= calcularFactorial; index++) {
+    resultadoFactorial = resultadoFactorial * index;
+  }
+  console.log(
+    'el factorial de ' + calcularFactorial + ' es ' + resultadoFactorial
+  );
+}
